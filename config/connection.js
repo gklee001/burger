68f96dd55cea73c86caf32bff981c50793c31d5e
Kneeslapper <gloriaklee8@gmail.com>
2019-12-06 21:21:27 -0800

    connection stuff

--- config/connection.js
+++ config/connection.js
@@ -9,17 +9,14 @@ var connection;
 //     database: "burgers_db"
 // });
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_RUL);
-} else {
-    connection = mysql.createConnection({
-        host: 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'kva8vl4a1156s3wb',
-        password: 'fdh17xgu0efiju8r',
-        database: 'fgkagewqwqbt1cas',
-        port: 3306
-    });
-};
+
+connection = mysql.createConnection({
+    host: 'lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'kva8vl4a1156s3wb',
+    password: 'fdh17xgu0efiju8r',
+    database: 'fgkagewqwqbt1cas',
+    port: 3306
+});
 
 
 connection.connect(function (err) {
